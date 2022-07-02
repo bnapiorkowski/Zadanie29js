86ad4c6929a6e2cf936a8350147df7703ed2a44b
bnapiorkowski <bartosz-napiorkowski@wp.pl>
2022-07-02 16:59:14 +0200

    Zadanie29js

---
+++ main.js
@@ -0,0 +1,8 @@
+let firstNumber = 3;
+let secondNumber = 10;
+function result (firstNumber,secondNumber){
+    let suma = firstNumber + secondNumber;
+    return suma;
+}
+console.log(result(3,10));
+
